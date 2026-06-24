99137d2bcc1369c1ba8480ff719bee8c8b9a8c0e
fanhuadesenlinnn <60951319+fanhuadesenlinnn@users.noreply.github.com>
2026-06-24 17:45:45 +0800

    Add backend make-upgrade interface

--- internal/backend/backend.go
+++ internal/backend/backend.go
@@ -11,6 +11,7 @@ type Backend interface {
 	Name() string
 	Check(context.Context, *config.ProfileConfig) error
 	Make(context.Context, *config.Config, *config.ProfileConfig, []string) error
+	MakeUpgrade(context.Context, *config.Config, *config.ProfileConfig) error
 	EnableLocalRepo(context.Context, *config.Config, *config.ProfileConfig) error
 	DisableLocalRepo(context.Context, *config.Config, *config.ProfileConfig, bool) error
 	GenerateClientRepo(*config.Config, *config.ProfileConfig, string) error
